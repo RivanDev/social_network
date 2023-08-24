b69aa3e00274a7553dea3ce4d202dbd801605fcb
Rivan <rivan_git@mail.ru>
2023-08-24 20:25:13 +0300

    add form registration

--- social_network/urls.py
+++ social_network/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .social_network import views as v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("register/", v.register_request, name="register"),
 ]
