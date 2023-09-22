fa5cfd85b8e4d5e42214ad04b37f761a4898793f
Rivan <rivan_git@mail.ru>
2023-09-22 22:43:08 +0300

    add form

--- buddy/urls.py
+++ buddy/urls.py
@@ -1,6 +1,9 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index),
-]
+    path('login/', views.sign_in, name='login'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- buddy/views.py
+++ buddy/views.py
@@ -1,6 +1,30 @@
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import LoginForm
 
 
 def index(request):
     return HttpResponse("Страница приложения buddy")
+
+
+def sign_in(request):
+    if request.method == 'GET':
+        form = LoginForm()
+        return render(request, 'buddy/login.html', {'form': form})
+    elif request.method == 'POST':
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                messages.success(request, f'Hi {username.title()}, welcome back!')
+                return redirect('posts')
+
+        # form is not valid or user is not authenticated
+        messages.error(request, f'Invalid username or password')
+        return render(request, 'buddy/login.html', {'form': form})
